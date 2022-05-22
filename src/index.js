9eb81ddf99082438536048b55eed89bd117799f2
QuentinDstl <qdesautel@gmail.com>
2022-05-22 15:07:22 +0530

    hot-reload depreciated : moving to fresh-reload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,77 @@
-const path = require("path");
-const webpack = require("webpack");
+// depreciated method using hot-relaod and not fast reload
+// const path = require("path");
+// const webpack = require("webpack");
+
+// module.exports = {
+// 	entry: "./src/index.js",
+// 	mode: "development",
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.(js|jsx)$/,
+// 				exclude: /(node_modules|bower_components)/,
+// 				loader: "babel-loader",
+// 				options: { presets: ["@babel/env"] }
+// 			},
+// 			{
+// 				test: /\.css$/,
+// 				use: ["style-loader", "css-loader"]
+// 			}
+// 		]
+// 	},
+// 	resolve: { extensions: ["*", ".js", ".jsx"] },
+// 	output: {
+// 		path: path.resolve(__dirname, "dist/"),
+// 		publicPath: "/dist/",
+// 		filename: "bundle.js"
+// 	},
+// 	devServer: {
+// 		static: {
+// 			directory: path.join(__dirname, "public/")
+// 		},
+// 		port: 3000,
+// 		devMiddleware: {
+// 			publicPath: "https://localhost:3000/dist/",
+// 		},
+// 		hot: "only",
+// 	}
+// };
+
+const path = require('path');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-	entry: "./src/index.js",
-	mode: "development",
+	mode: isDevelopment ? 'development' : 'production',
+	devServer: {
+		client: { overlay: false },
+	},
+	entry: {
+		main: './src/index.js',
+	},
 	module: {
 		rules: [
 			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: "babel-loader",
-				options: { presets: ["@babel/env"] }
+				test: /\.jsx?$/,
+				include: path.join(__dirname, 'src'),
+				use: 'babel-loader',
 			},
 			{
 				test: /\.css$/,
 				use: ["style-loader", "css-loader"]
 			}
-		]
+		],
 	},
-	resolve: { extensions: ["*", ".js", ".jsx"] },
-	output: {
-		path: path.resolve(__dirname, "dist/"),
-		publicPath: "/dist/",
-		filename: "bundle.js"
+	plugins: [
+		isDevelopment && new ReactRefreshPlugin(),
+		new HtmlWebpackPlugin({
+			filename: './index.html',
+			template: './public/index.html',
+		}),
+	].filter(Boolean),
+	resolve: {
+		extensions: ['.js', '.jsx'],
 	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "public/")
-		},
-		port: 3000,
-		devMiddleware: {
-			publicPath: "https://localhost:3000/dist/",
-		},
-		hot: "only",
-	}
 };
